43726a883da3221b0d7b37d6809f29b4e6f16d4f
NguyenNhatVuong99 <nhatvuong0699@gmail.com>
2024-04-14 10:03:50 +0700

    update

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -25,7 +25,7 @@ STATICFILES_DIRS = [
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--dh=zcwsqk$zowx%_0z6yl&&shd0lon1!=0(3h@r0u+ym(1k(+'
+# SECRET_KEY = 'django-insecure--dh=zcwsqk$zowx%_0z6yl&&shd0lon1!=0(3h@r0u+ym(1k(+'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', "false").lower() == 'true'
